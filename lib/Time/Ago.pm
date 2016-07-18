84a0146373d649ef7a155b111b0e3b590f92378f
Maurice Aubrey <maurice.aubrey@gmail.com>
2016-07-18 12:33:46 -0700

    update version

--- lib/Time/Ago.pm
+++ lib/Time/Ago.pm
@@ -13,7 +13,7 @@ use Locale::Messages qw/ bind_textdomain_filter /;
 use Locale::TextDomain 'Time-Ago';
 use Scalar::Util qw/ blessed /;
 
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 
 BEGIN {
   $ENV{OUTPUT_CHARSET} = 'UTF-8';
